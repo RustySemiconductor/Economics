fde00d1c9f0f6deebdeec371c97f60f608f642fe
RustySemiconductor <rustysemiconductor@gmail.com>
2016-04-22 00:09:09 +0300

    Update technology.lua

--- prototypes/technology/technology.lua
+++ prototypes/technology/technology.lua
@@ -1,3 +1,28 @@
-data:extend({
-
-})
+data:extend(
+{
+    {
+    type = "technology",
+    name = "basic-economics",
+    icon = "__economics__/graphics/technology/basic-economics.png",
+    effects =
+    {
+      {
+        type = "unlock-recipe",
+        recipe = "blah-blah-1"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "blah-blah-2"
+      },
+    },
+    prerequisites = {"automation", "electronics"},
+    unit =
+    {
+      count = 20,
+      ingredients = {{"science-pack-1", 1}},
+      time = 15
+    },
+    order = "a-d-b"
+  },
+}
+)
