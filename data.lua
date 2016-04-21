1ecd7bb0a2f6b0390f385eb0f5bf88b37a195877
RustySemiconductor <rustysemiconductor@gmail.com>
2016-04-22 00:32:48 +0300

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,4 @@
+require("prototypes.item.economics-item")
+require("prototypes.technology.economics-technology")
+require("prototypes.entity.economics-entity")
+require("prototypes.recipe.economics-recipe")
